45e5aac6036a3175313e839ec59b7b1c2f8539c9
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-05-28 15:59:20 -0400

    Clean up cities JSON before sending response

--- README.md
+++ README.md
@@ -3,10 +3,28 @@ Environment Canada Weather API is a Node.JS Express App that provides simple end
 from Environment Canada.
 
 ### Endpoints
-/cities - Return city ID, province code and english/french names of all cities which Environment Canada provides 
+**/cities** - Return city ID, province code and english/french names of all cities which Environment Canada provides 
 forecasts for.
 
-/forecast?cityId={city ID}&province={province code}&language={language, e for english, f for french} - Return's 
+Sample Response (Returns 855 cities as of May 28, 2019):
+```json
+[
+  {
+    "cityId": "s0000001",
+    "englishName": "Athabasca",
+    "frenchName": "Athabasca",
+    "provinceCode": "AB"
+  },
+  {
+    "cityId": "s0000002",
+    "englishName": "Clearwater",
+    "frenchName": "Clearwater",
+    "provinceCode": "BC"
+  }
+]
+```
+
+**/forecast?cityId={city ID}&province={province code}&language={language, e for english, f for french}** - Return's 
 Environment Canada's Forecast as JSON for the given city.
 
 All responses are cached for 5 minutes.
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mcache from 'memory-cache';
-import xmlDownloader from './xml/xmldownloader';
+import xmlDownloader from './xml/xml-downloader';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,36 +9,44 @@ const cityUrl = 'http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/siteList.
 const forecastUrl = 'http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/';
 
 const cacheSeconds = 300;
-const cache = () => {
-    return (req, res, next) => {
-        const key = '__express__' + req.originalUrl || req.url;
-        const cachedBody = mcache.get(key);
+app.use((req, res, next) => {
+    const key = '__express__' + req.originalUrl || req.url;
+    const cachedBody = mcache.get(key);
 
-        if (cachedBody) {
-            res.send(cachedBody);
-        } else {
-            res.sendResponse = res.send;
-            res.send = (body) => {
-                mcache.put(key, body, cacheSeconds * 1000);
-                res.sendResponse(body)
-            };
-            next()
-        }
+    if (cachedBody) {
+        res.send(cachedBody);
+    } else {
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            mcache.put(key, body, cacheSeconds * 1000);
+            res.sendResponse(body)
+        };
+        next()
     }
-};
+});
 
-app.get('/cities', cache(), (req, res) => {
+app.get('/cities', (req, res) => {
     xmlDownloader(cityUrl, (error, result) => {
        if (error) {
            res.sendStatus(502);
            return;
        }
 
-       res.json(result);
+       let cities = [];
+       result['siteList']['site'].forEach((e) => {
+           cities.push({
+              'cityId': e['$']['code'],
+              'englishName': e['nameEn'][0],
+              'frenchName': e['nameFr'][0],
+              'provinceCode': e['provinceCode'][0]
+           });
+       });
+
+       res.json(cities);
     });
 });
 
-app.get('/forecast', cache(), (req, res) => {
+app.get('/forecast', (req, res) => {
     const provinceCode = req.query.province;
     const cityId = req.query.cityId;
     const language = req.query.language;
